e78eaf187e3de1c003f8f4f5495d0754d3601b0b
aoruilin0208 <646010544@qq.com>
2019-05-12 20:40:40 +0800

    更换路径

--- sign/models.py
+++ sign/models.py
@@ -13,3 +13,19 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# 嘉宾表
+class Guest(models.Model):
+    event = models.ForeignKey(Event)  # 关联发布会id
+    realname = models.CharField(max_length=64)  # 姓名
+    phone = models.CharField(max_length=64)  # 手机号
+    email = models.EmailField()  # 邮箱
+    sign = models.BooleanField()  # 签到状态
+    create_time = models.DateTimeField(auto_now=True)  # 签到时间（自动获取当前时间）
+
+    class Meta:
+        unique_together = ('event', 'phone')
+
+    def __str__(self):
+        return self.realname
